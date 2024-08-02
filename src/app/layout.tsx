f6dbbd07f9c3d4d76d48984e145a31906108ac25
M0rl0ck <seriy17@yandex.ru>
2024-08-02 22:13:22 +0300

    init: start project

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Container } from "@mui/material";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Test task for AVK GROUP",
+  description: "This app is a test task for AVK GROUP",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main>
+          <Container maxWidth="xl">{children}</Container>
+        </main>
+      </body>
+    </html>
+  );
+}
