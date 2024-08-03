537031ffa34a853c92ce32036a845eae1830dea2
M0rl0ck <seriy17@yandex.ru>
2024-08-03 18:14:13 +0300

    feat: add pagination

--- src/app/(startPage)/page.tsx
+++ src/app/(startPage)/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   const { data, isLoading, isError } = usePosts();
   return (
     <>
-      <h1>Hello</h1>
       {data?.length && <PostsLists posts={data} />}
 
       {isLoading && <p>Loading...</p>}
--- src/components/PostsList/PostsLists.tsx
+++ src/components/PostsList/PostsLists.tsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import Stack from "@mui/material/Stack";
 import { IPost } from "@/types/IPost";
-import Divider from "@mui/material/Divider/Divider";
 import Post from "../Post/Post";
+import { Pagination, PaginationItem } from "@mui/material";
 
+const RANGE = 20;
 interface IPostsLists {
   posts: IPost[];
 }
 
 const PostsLists = ({ posts }: IPostsLists) => {
+  const currentPage = useSearchParams().get("page");
+  const [page, setPage] = useState(currentPage ? Number(currentPage) - 1 : 0);
+  const [postsList, setPostsList] = useState(
+    posts.slice(page * RANGE, page * RANGE + RANGE)
+  );
+
+  useEffect(() => {
+    setPostsList(posts.slice(page * RANGE, page * RANGE + RANGE));
+  }, [page, posts]);
+
   return (
     <Stack justifyContent="center" alignItems="center" spacing={4}>
-      {posts.map((post) => (
+      <Pagination
+        count={Math.ceil(posts.length / RANGE)}
+        defaultPage={page + 1}
+        variant="outlined"
+        color="primary"
+        onChange={(_, value) => setPage(value - 1)}
+        renderItem={(item) => (
+          <PaginationItem
+            component={Link}
+            href={`/?page=${item.page ? item.page : 1}`}
+            {...item}
+          />
+        )}
+      />
+      {postsList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </Stack>
